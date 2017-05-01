69054500ba3d8680c3098167646a40a954a39034
Adrian Kubik <akubik@inovatica.com>
2017-05-01 16:47:15 +0200

    Initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="{{ config('app.locale') }}">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Laravel</title>
+
+    <!-- Fonts -->
+    <link href="/css/app.css" rel="stylesheet">
+
+
+</head>
+<body>
+
+<h1>Scheduler</h1>
+</body>
+</html>
