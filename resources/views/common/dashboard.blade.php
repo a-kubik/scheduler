9ec5a48b2cbe5885470fffd88606f4c9418b5d40
Adrian Kubik <akubik@inovatica.com>
2017-05-08 22:04:44 +0200

    Dashboard prepared

---
+++ resources/views/common/dashboard.blade.php
@@ -0,0 +1,34 @@
+@extends('layouts.master')
+
+@section('content')
+
+    <div class="sidebar">
+        <div class="user-avatar">
+            <img src="/avatar.jpg">
+            <p class="username">Bellissima Antonia</p>
+        </div>
+
+        <ul class="menu">
+            {{--@if(Auth::user()->isAdmin())
+                <li><a href="/admin">Admin panel</a></li>
+            @endif--}}
+            <li><a href="{{url('/dashboard')}}">Dashboard</a></li>
+            <li><a href="{{url('/tasks')}}">Tasks</a></li>
+            <li><a href="{{url('/settings')}}">Settings</a></li>
+            <li><a href="{{url('/notes')}}">Notes</a></li>
+            <li><a href="">Messages</a></li>
+            <li><a href="">News</a></li>
+        </ul>
+
+
+    </div>
+
+
+    <div class="content">
+        @yield('inner')
+    </div>
+
+
+
+
+@endsection
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Laravel</title>
+    <title>Scheduler</title>
 
     <!-- Fonts -->
     <link href="/css/app.css" rel="stylesheet">
@@ -15,5 +15,7 @@
 <body>
 
 <h1>Scheduler</h1>
+
+@yield('content')
 </body>
 </html>
